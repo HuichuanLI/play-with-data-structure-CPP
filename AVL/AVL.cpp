cb44bd0c5db9ce7f7ed6de9079e8c76e0c3e2bd2
lhc456 <lhc14124908@163.com>
2023-04-19 01:11:43 +0800

    add

--- AVL/AVL.cpp
+++ AVL/AVL.cpp
@@ -8,48 +8,44 @@ using namespace std;
 
 
 //链式栈中每个节点的定义
-template <typename T> //T代表数据元素的类型
-struct StackNode
-{
-    T        data;  //数据域，存放数据元素
-    StackNode<T>* next;  //指针域，指向下一个同类型（和本节点类型相同）节点
+template<typename T> //T代表数据元素的类型
+struct StackNode {
+    T data;  //数据域，存放数据元素
+    StackNode<T> *next;  //指针域，指向下一个同类型（和本节点类型相同）节点
 };
 
 //链式栈的定义
-template <typename T>
-class LinkStack
-{
+template<typename T>
+class LinkStack {
 public:
     LinkStack();      //构造函数，参数可以有默认值
     ~LinkStack();     //析构函数
 
 public:
-    bool Push(const T& e); //入栈元素e
-    bool Pop(T& e);        //出栈（删除数据），也就是删除栈顶数据
-    bool GetTop(T& e);     //读取栈顶元素，但该元素并没有出栈而是依旧在栈中
+    bool Push(const T &e); //入栈元素e
+    bool Pop(T &e);        //出栈（删除数据），也就是删除栈顶数据
+    bool GetTop(T &e);     //读取栈顶元素，但该元素并没有出栈而是依旧在栈中
 
     void DispList();                     //输出链式栈中的所有元素
-    int  ListLength();                   //获取链式栈的长度
+    int ListLength();                   //获取链式栈的长度
     bool Empty();                        //判断链式栈是否为空
 
 private:
-    StackNode<T>* m_top;   //栈顶指针
-    int               m_length;//链式栈当前长度
+    StackNode<T> *m_top;   //栈顶指针
+    int m_length;//链式栈当前长度
 };
 
 //通过构造函数对链式栈进行初始化
-template <typename T>
-LinkStack<T>::LinkStack()
-{
+template<typename T>
+LinkStack<T>::LinkStack() {
     m_top = nullptr;
     m_length = 0;
 }
 
 //入栈元素e，时间复杂度为O(1)
-template <typename T>
-bool LinkStack<T>::Push(const T& e)
-{
-    StackNode<T>* node = new StackNode<T>;
+template<typename T>
+bool LinkStack<T>::Push(const T &e) {
+    StackNode<T> *node = new StackNode<T>;
     node->data = e;
     node->next = m_top;
     m_top = node;
@@ -58,13 +54,12 @@ bool LinkStack<T>::Push(const T& e)
 }
 
 //出栈（删除数据），也就是删除栈顶数据，时间复杂度为O(1)
-template <typename T>
-bool LinkStack<T>::Pop(T& e)
-{
+template<typename T>
+bool LinkStack<T>::Pop(T &e) {
     if (Empty() == true) //链式栈为空
         return false;
 
-    StackNode<T>* p_willdel = m_top;
+    StackNode<T> *p_willdel = m_top;
     m_top = m_top->next;
     m_length--;
     e = p_willdel->data;
@@ -73,9 +68,8 @@ bool LinkStack<T>::Pop(T& e)
 }
 
 //读取栈顶元素，但该元素并没有出栈而是依旧在栈中
-template <typename T>
-bool LinkStack<T>::GetTop(T& e)
-{
+template<typename T>
+bool LinkStack<T>::GetTop(T &e) {
     if (Empty() == true) //链式栈为空
         return false;
 
@@ -85,14 +79,12 @@ bool LinkStack<T>::GetTop(T& e)
 
 //输出链式栈中的所有元素，时间复杂度为O(n)
 template<class T>
-void LinkStack<T>::DispList()
-{
+void LinkStack<T>::DispList() {
     if (Empty() == true) //链式栈为空
         return;
 
-    StackNode<T>* p = m_top;
-    while (p != nullptr)
-    {
+    StackNode<T> *p = m_top;
+    while (p != nullptr) {
         cout << p->data << " ";  //每个数据之间以空格分隔
         p = p->next;
     }
@@ -101,15 +93,13 @@ void LinkStack<T>::DispList()
 
 //获取链式栈的长度，时间复杂度为O(1)
 template<class T>
-int  LinkStack<T>::ListLength()
-{
+int LinkStack<T>::ListLength() {
     return m_length;
 }
 
 //判断链式栈是否为空，时间复杂度为O(1)
 template<class T>
-bool LinkStack<T>::Empty()
-{
+bool LinkStack<T>::Empty() {
     if (m_top == nullptr) //链式栈为空
     {
         return true;
@@ -118,55 +108,52 @@ bool LinkStack<T>::Empty()
 }
 
 //通过析构函数对链式栈进行资源释放
-template <typename T>
-LinkStack<T>::~LinkStack()
-{
-    T tmpnousevalue = { 0 };
+template<typename T>
+LinkStack<T>::~LinkStack() {
+    T tmpnousevalue = {0};
     while (Pop(tmpnousevalue) == true) {}//把栈顶元素删光，while循环也就退出了，此时也就是空栈了
 }
 
 //--------------------------------------
 //平衡二叉树中每个节点的定义
-template <typename T> //T代表数据元素的类型
-struct AVLNode
-{
-    T        data;
-    AVLNode* leftChild,   //左子节点指针
-    * rightChild;  //右子节点指针
-    int      balfac;      //平衡因子
+template<typename T> //T代表数据元素的类型
+struct AVLNode {
+    T data;
+    AVLNode *leftChild,   //左子节点指针
+    *rightChild;  //右子节点指针
+    int balfac;      //平衡因子
 };
 
 //平衡二叉树的定义
-template <typename T>
-class AVLTree
-{
+template<typename T>
+class AVLTree {
 public:
     AVLTree()  //构造函数
     {
         root = nullptr;
     }
+
     ~AVLTree() //析构函数
     {
         ReleaseNode(root);
     }
 
     //插入元素
-    void InsertElem(const T& e)  //不可以指定插入位置，程序内部会自动确定插入位置
+    void InsertElem(const T &e)  //不可以指定插入位置，程序内部会自动确定插入位置
     {
         InsertElem(root, e);
     }
 
-    void InsertElem(AVLNode<T>*& tNode, const T& e) //注意第一个参数类型
+    void InsertElem(AVLNode<T> *&tNode, const T &e) //注意第一个参数类型
     {
-        AVLNode<T>* point = tNode;     //从指向根节点开始
-        AVLNode<T>* parent = nullptr;  //保存父亲节点，根节点的父节点肯定先为nullptr
+        AVLNode<T> *point = tNode;     //从指向根节点开始
+        AVLNode<T> *parent = nullptr;  //保存父亲节点，根节点的父节点肯定先为nullptr
 
         //借助以往实现的栈（链栈）代码保存插入的节点路径信息（用于后面调节平衡因子、调整平衡）
-        LinkStack< AVLNode<T>* > slinkobj;
+        LinkStack<AVLNode<T> *> slinkobj;
 
         //通过一个while循环寻找要插入节点的位置，同时还要把插入路线上所经过的所有节点都保存到栈中，因为这些节点的平衡因子可能需要调整
-        while (point != nullptr)
-        {
+        while (point != nullptr) {
             if (e == point->data)
                 return;   //要插入的数据与当前树中某节点数据相同，则不允许插入，直接返回
 
@@ -186,8 +173,7 @@ public:
         point->rightChild = nullptr;
         point->balfac = 0; //插入时肯定是个叶节点，所以平衡因子肯定等于0
 
-        if (parent == nullptr)
-        {
+        if (parent == nullptr) {
             //创建的是根节点
             tNode = point;
             return;
@@ -200,8 +186,7 @@ public:
             parent->leftChild = point;  //在父节点的左树链入
 
         //下面即将修改平衡因子 以及 调整平衡，前面栈slinkobj里的节点就是可能要更改平衡因子和做平衡调整的节点
-        while (slinkobj.Empty() != true)
-        {
+        while (slinkobj.Empty() != true) {
             if (slinkobj.Pop(parent) == true) //取栈顶元素到parent中，第一次获取到的是新插入节点的父节点
             {
                 //(1)因为插入了孩子，所以 调整父的平衡因子
@@ -211,24 +196,20 @@ public:
                     parent->balfac--;  //平衡因子-1
 
                 //(2)找最小不平衡子树的根节点 并进行平衡性调整
-                if (parent->balfac < -1 || parent->balfac > 1)
-                {
+                if (parent->balfac < -1 || parent->balfac > 1) {
                     //有四种旋转方式，采取哪种旋转方式呢？
 
                     //(1)如果 这棵最小不平衡子树 的根节点 和其孩子节点 的平衡因子都 >0，那就是LL情形，要右旋转来恢复平衡
-                    if (parent->balfac > 0 && point->balfac > 0)
-                    {
+                    if (parent->balfac > 0 && point->balfac > 0) {
                         RotateRight(parent);
                     }
                         //(2)如果 这棵最小不平衡子树 的根节点 和其孩子节点 的平衡因子都 < 0，那就是RR情形，要左旋转来恢复平衡
-                    else if (parent->balfac < 0 && point->balfac < 0)
-                    {
+                    else if (parent->balfac < 0 && point->balfac < 0) {
                         RotateLeft(parent);
                     }
 
                         //(3)如果 这棵最小不平衡子树 的根节点 的平衡因子>0  其孩子节点 的平衡因子都 <0，那就是LR情形，要先左旋再右旋来恢复平衡
-                    else if (parent->balfac > 0 && point->balfac < 0)
-                    {
+                    else if (parent->balfac > 0 && point->balfac < 0) {
                         RotateLeftRight(parent);
                     }
 
@@ -239,15 +220,12 @@ public:
                     }
 
                     //根相关指针的调整
-                    if (slinkobj.Empty() == true)
-                    {
+                    if (slinkobj.Empty() == true) {
                         //本条件成立，表示本次平衡性调整调整到了整个树最上面的根节点，因为平衡性调整会使树根节点发生改变，所以这里要更新根节点的指向
                         root = parent;
-                    }
-                    else
-                    {
+                    } else {
                         //本次平衡性调整 并没有调整到 整个树最上面的根节点，但 因为平衡性调整会使树根节点发生改变，所以老根节点的孩子指针应该指向新根节点
-                        AVLNode<T>* pParentPoint = nullptr;
+                        AVLNode<T> *pParentPoint = nullptr;
                         slinkobj.GetTop(pParentPoint);  //拿到老根的父节点，一定会取得成功，因为栈不为空
 
                         //判断让老根父节点（pParentPoint）的左孩子指针还是右孩子指针指向新根（parent）
@@ -261,14 +239,10 @@ public:
 
                     //比如某个节点A有个左子树B1，所以其平衡因子为1,现在给其增加一个右子树B2，其平衡因子变为0，
                     //此时不要再继续向上调整节点A的父的平衡因子了，否则会把A父亲的平衡因子调整错误。
-                else if (parent->balfac == 0)
-                {
+                else if (parent->balfac == 0) {
                     //已经平衡，不需要继续 回溯 调整
                     break;
-                }
-
-                else
-                {
+                } else {
                     point = parent;   //让point指向父节点，后序parent的新值要从栈顶获取
                 } //end if (parent->balfac < -1 || parent->balfac > 1)
 
@@ -279,17 +253,17 @@ public:
     }
 
     //删除某个节点
-    void DeleteElem(const T& e)
-    {
+    void DeleteElem(const T &e) {
         return DeleteElem(root, e);
     }
-    void DeleteElem(AVLNode<T>*& tNode, const T& e)  //注意第一个参数类型
+
+    void DeleteElem(AVLNode<T> *&tNode, const T &e)  //注意第一个参数类型
     {
-        AVLNode<T>* ptmp = tNode; //要删除的节点
-        AVLNode<T>* parent = nullptr;  //保存父亲节点，根节点的父节点肯定先为nullptr
+        AVLNode<T> *ptmp = tNode; //要删除的节点
+        AVLNode<T> *parent = nullptr;  //保存父亲节点，根节点的父节点肯定先为nullptr
 
         //借助栈保存删除的节点路径信息
-        LinkStack< AVLNode<T>* > slinkobj;
+        LinkStack<AVLNode<T> *> slinkobj;
 
         while (ptmp != nullptr) //通过while循环尝试让ptmp指向要被删除的节点
         {
@@ -308,20 +282,16 @@ public:
             return;
 
         //找到了要删除的节点，前面二叉查找树删除节点分几种情况：
-        AVLNode<T>* q = nullptr;         //临时指针变量
-        if (ptmp->leftChild == nullptr && ptmp->rightChild == nullptr)
-        {
+        AVLNode<T> *q = nullptr;         //临时指针变量
+        if (ptmp->leftChild == nullptr && ptmp->rightChild == nullptr) {
             //如果要删除的节点左子树和右子树都为空（叶节点）
-            if (parent != nullptr)
-            {
+            if (parent != nullptr) {
                 if (parent->leftChild == ptmp) //要删除的节点是其父的左孩子
                     parent->leftChild = nullptr;
                 else
                     parent->rightChild = nullptr;
             }
-        }
-        else if (ptmp->leftChild != nullptr && ptmp->rightChild != nullptr)
-        {
+        } else if (ptmp->leftChild != nullptr && ptmp->rightChild != nullptr) {
             //如果要删除的节点左子树和右子树都不为空，则把对当前节点的删除转换为对当前节点左子树的最右下节点的删除
             //这里涉及到的问题是要记录最终真删除的节点的路径
             //删除举例：比如删除如下的D节点，最后会转变成删除F节点。ptmp指向的是F节点
@@ -334,8 +304,7 @@ public:
             slinkobj.Push(parent); //入栈
             //(2)找到这个要删除节点的左子树的最右下节点（也可以找这个要删除节点右子树的最左下节点）,将该节点的值替换到要删除的节点上；
             q = ptmp->leftChild;
-            while (q->rightChild != nullptr)
-            {
+            while (q->rightChild != nullptr) {
                 parent = q;
                 slinkobj.Push(parent); //入栈
                 q = q->rightChild;
@@ -344,24 +313,20 @@ public:
             ptmp = q; //让ptmp指向真正删除的节点，也就是把删除一个既有左子树右有右子树的节点转化为删除一个叶子节点。
 
             //上面找到这个节点肯定没有右子树，因为找的是左子树的最右下节点嘛！
-            if (parent != nullptr)
-            {
+            if (parent != nullptr) {
                 if (parent->leftChild == ptmp)
                     parent->leftChild = ptmp->leftChild;
                 else
                     parent->rightChild = ptmp->leftChild;
             }
-        }
-        else
-        {
+        } else {
             //如果要删除的节点的左子树为空或者右子树为空(两者肯定有一个为空才能走到这个分支)，让q指向不空的孩子节点
             if (ptmp->leftChild != nullptr)
                 q = ptmp->leftChild;
             else
                 q = ptmp->rightChild;
 
-            if (parent != nullptr)
-            {
+            if (parent != nullptr) {
                 //把被删除的节点的子节点链接到被删除节点的父节点上去
                 if (parent->leftChild == ptmp) //要删除的节点是其父的左孩子
                     parent->leftChild = q;
@@ -378,8 +343,7 @@ public:
                 tNode = nullptr;
             else if (ptmp->leftChild == nullptr || ptmp->rightChild == nullptr) //要删除这棵树的根节点并且这棵树根的左子树为空或者右子树为空
                 tNode = q;
-            else
-            {
+            else {
                 //这个else条件应该一直不会成立
                 assert(false);//记得#include <cassert>,assert(false);意味着代码不可能执行到这条语句，若执行到了，会报告异常
             }
@@ -387,10 +351,8 @@ public:
 
         //--------------------------------------------------------------
         //处理平衡因子的改变（平衡性调整）
-        while (slinkobj.Empty() != true)
-        {
-            if (slinkobj.Pop(parent) == true)
-            {
+        while (slinkobj.Empty() != true) {
+            if (slinkobj.Pop(parent) == true) {
                 //如果删除的是叶子节点，并且其父亲只有一个叶子，那么删除后，其父亲就变成叶子了，叶子的平衡因子自然为0
                 if (parent->leftChild == nullptr && parent->rightChild == nullptr)
                     parent->balfac = 0;
@@ -399,21 +361,18 @@ public:
                 else //删除右树
                     parent->balfac++; //平衡因子增加
 
-                if (parent->balfac == -1 || parent->balfac == 1)
-                {
+                if (parent->balfac == -1 || parent->balfac == 1) {
                     //说明parent节点原来的平衡因子为0，也就是原来左右孩子都有，那么删除任意一个孩子，除parent节点平衡因子发生变化外，任何其他的parent的父亲等节点的平衡因子不会发生变化，这里可以直接退出
                     break;
                 }
-                if (parent->balfac == 0)
-                {
+                if (parent->balfac == 0) {
                     //说明parent节点原来的平衡因子为-1或者1，得继续回溯向上尝试调整其他父节点平衡因子
                     q = parent;
                     continue;
                 }
 
                 //根据规则来
-                if (parent->balfac == -2)
-                {
+                if (parent->balfac == -2) {
                     //平衡因子为-2，所以能确定的是，一定有右孩子
                     if (parent->rightChild->balfac == -1) //左旋转
                         RotateLeft(parent);
@@ -427,9 +386,7 @@ public:
                         parent->balfac = 1;
                         parent->leftChild->balfac = -1;
                     }
-                }
-                else
-                {
+                } else {
                     //平衡因子为2，所以能确定的是，一定有左孩子
                     if (parent->leftChild->balfac == -1) //先左后右旋转
                         RotateLeftRight(parent);
@@ -446,15 +403,12 @@ public:
                 } //end if (parent->balfac == -2)
 
                 //根相关指针的调整
-                if (slinkobj.Empty() == true)
-                {
+                if (slinkobj.Empty() == true) {
                     //本条件成立，表示本次平衡性调整调整到了整个树最上面的根节点，因为平衡性调整会使树根节点发生改变，所以这里要更新根节点的指向
                     root = parent;
-                }
-                else
-                {
+                } else {
                     //本次平衡性调整并没有调整到整个树最上面的根节点，但因为平衡性调整会使树根节点发生改变，所以老根节点的孩子指针应该指向新根节点
-                    AVLNode<T>* pParentPoint = nullptr;
+                    AVLNode<T> *pParentPoint = nullptr;
                     slinkobj.GetTop(pParentPoint);  //拿到老根的父节点，一定会取得成功，因为栈不为空
 
                     //判断让老根父节点（pParentPoint）的左孩子指针还是右孩子指针指向新根（parent）
@@ -471,9 +425,9 @@ public:
 
 private:
     //左旋转（RR插入导致失衡）
-    void RotateLeft(AVLNode<T>*& pointer)//注意参数类型
+    void RotateLeft(AVLNode<T> *&pointer)//注意参数类型
     {
-        AVLNode<T>* childLeft = pointer; //pointer是 “最小不平衡子树”的根节点，即 失衡的节点A
+        AVLNode<T> *childLeft = pointer; //pointer是 “最小不平衡子树”的根节点，即 失衡的节点A
         pointer = childLeft->rightChild;  //新根（节点B） = 老根（节点A）的右孩子节点
 
         childLeft->rightChild = pointer->leftChild;  //如果节点B原来有左孩子，则首先将节点B这个左孩子作为节点A的右孩子。
@@ -484,9 +438,9 @@ private:
     }
 
     //右旋转（LL插入导致失衡）
-    void RotateRight(AVLNode<T>*& pointer) //注意参数类型
+    void RotateRight(AVLNode<T> *&pointer) //注意参数类型
     {
-        AVLNode<T>* childRight = pointer; //pointer是 “最小不平衡子树”的根节点，即 失衡的节点A
+        AVLNode<T> *childRight = pointer; //pointer是 “最小不平衡子树”的根节点，即 失衡的节点A
         pointer = childRight->leftChild;  //新根（节点B） = 老根（节点A）的左孩子节点
 
         childRight->leftChild = pointer->rightChild;  //如果节点B原来有右孩子，则首先将节点B这个右孩子作为节点A的左孩子。
@@ -497,11 +451,10 @@ private:
     }
 
     //先左后右旋转（LR插入导致失衡）
-    void RotateLeftRight(AVLNode<T>*& pointer)
-    {
+    void RotateLeftRight(AVLNode<T> *&pointer) {
         //给值，让各个变量指向不同的节点
-        AVLNode<T>* childRight = pointer;   //pointer是 “最小不平衡子树”的根节点，即 失衡的节点A
-        AVLNode<T>* childLeft = childRight->leftChild;  //节点B = 节点A 的左孩子节点
+        AVLNode<T> *childRight = pointer;   //pointer是 “最小不平衡子树”的根节点，即 失衡的节点A
+        AVLNode<T> *childLeft = childRight->leftChild;  //节点B = 节点A 的左孩子节点
         pointer = childLeft->rightChild;    //节点C，就是节点B的右孩子，也是最终 先左后右旋转后的新根
 
         //进行左旋转
@@ -517,7 +470,7 @@ private:
         //进行上述的左旋转后，新的节点B其实已经是pointer了。而且此时节点C（新根）的平衡因子还保持为老值（1或者-1）
         //再进行右旋转
         childRight->leftChild = pointer->rightChild; //如果节点C原来有右孩子，则将这个右孩子变成节点A的左孩子
-        pointer->rightChild = childRight;	//将失衡的节点A向右侧旋转来作为【新】节点B（pointer）的右孩子
+        pointer->rightChild = childRight;    //将失衡的节点A向右侧旋转来作为【新】节点B（pointer）的右孩子
         if (pointer->balfac == 1)    //新节点B原来带的是左孩子
             childRight->balfac = -1; //节点A原来有个右孩子，所以节点A平衡因子变成-1
         else
@@ -528,10 +481,10 @@ private:
     }
 
     //先右后左旋转（RL插入导致失衡），参考RotateLeftRight()书写
-    void RotateRightLeft(AVLNode<T>*& pointer)  //注意参数类型
+    void RotateRightLeft(AVLNode<T> *&pointer)  //注意参数类型
     {
-        AVLNode<T>* childLeft = pointer;  //以课程/书中实际节点来说明：60
-        AVLNode<T>* childRight = childLeft->rightChild;  //120
+        AVLNode<T> *childLeft = pointer;  //以课程/书中实际节点来说明：60
+        AVLNode<T> *childRight = childLeft->rightChild;  //120
         pointer = childRight->leftChild;    //95
 
         //进行右旋转
@@ -557,10 +510,8 @@ private:
         pointer->balfac = 0;  //95这个节点的平衡因子设置为0
     }
 
-    void ReleaseNode(AVLNode<T>* pnode)
-    {
-        if (pnode != nullptr)
-        {
+    void ReleaseNode(AVLNode<T> *pnode) {
+        if (pnode != nullptr) {
             ReleaseNode(pnode->leftChild);
             ReleaseNode(pnode->rightChild);
         }
@@ -568,15 +519,14 @@ private:
     }
 
 private:
-    AVLNode<T>* root; //树根指针
+    AVLNode<T> *root; //树根指针
 };
 //-----------------------------------------
 
-int main()
-{
+int main() {
 
     AVLTree<int> mybtr;
-    int array[] = { 60,31,65,15,42,62,75,12,25,37,50,63,69,85,2,32,38,48,56,82,34 };
+    int array[] = {60, 31, 65, 15, 42, 62, 75, 12, 25, 37, 50, 63, 69, 85, 2, 32, 38, 48, 56, 82, 34};
     int acount = sizeof(array) / sizeof(int);
     for (int i = 0; i < acount; ++i)
         mybtr.InsertElem(array[i]);
